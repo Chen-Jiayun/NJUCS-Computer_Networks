7889a403b55d928ce88ed2b6120d6166b7a2a7e3
Jiayun Chen <jiayunchen@smail.nju.edu.cn>
2023-09-14 23:09:54 +0800

    lab 0 raw done

--- sponge/apps/webget.cc
+++ sponge/apps/webget.cc
@@ -17,8 +17,17 @@ void get_URL(const string &host, const string &path) {
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
 
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    // cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    // cerr << "Warning: get_URL() has not been implemented yet.\n";
+    TCPSocket socket;
+    socket.connect(Address(host, "http"));
+    std::string cmd = "GET " + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" 
+                        + "Connection: close\r\n";
+    socket.write(cmd);
+    while(socket.eof()) {
+        cout << socket.read();
+    }
+    socket.shutdown(SHUT_RDWR);
 }
 
 int main(int argc, char *argv[]) {
