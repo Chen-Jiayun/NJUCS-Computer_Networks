84b94bfab503767516ba71e8046dfcb3ca1eb4cd
Jiayun Chen <jiayunchen@smail.nju.edu.cn>
2023-10-12 11:20:58 +0800

    sync lab1

--- sponge/libsponge/byte_stream.hh
+++ sponge/libsponge/byte_stream.hh
@@ -3,12 +3,16 @@
 
 #include <string>
 
+// for local test
+class Test;
+
 //! \brief An in-order byte stream.
 
 //! Bytes are written on the "input" side and read from the "output"
 //! side.  The byte stream is finite: the writer can end the input,
 //! and then no more bytes can be written.
 class ByteStream {
+    friend Test; // local test will check private variable directly.
   private:
     // Your code here -- add private members as necessary.
 
@@ -18,7 +22,18 @@ class ByteStream {
     // different approaches.
 
     bool _error{};  //!< Flag indicating that the stream suffered an error.
+    
+    std::string buffer;
 
+    const size_t _capacity;
+
+    bool _input_end; // default false
+
+    size_t _curr_size; // current used bytes
+
+    size_t _written;
+
+    size_t _read;
   public:
     //! Construct a stream with room for `capacity` bytes.
     ByteStream(const size_t capacity);
