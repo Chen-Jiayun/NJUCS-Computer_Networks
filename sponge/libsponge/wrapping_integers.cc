4055588ead9e664476ec4a0f4c5d8cf114d4b600
Jiayun Chen <jiayunchen@smail.nju.edu.cn>
2023-11-01 15:00:19 +0800

    compile done

--- sponge/libsponge/wrapping_integers.cc
+++ sponge/libsponge/wrapping_integers.cc
@@ -1,5 +1,7 @@
 #include "wrapping_integers.hh"
 
+#include <assert.h>
+
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -10,12 +12,37 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
+using u64 = uint64_t;
+using u32 = uint32_t;
+
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return WrappingInt32(n + isn.raw_value());
+}
+
+static u64 min3(u64 x, u64 y, u64 z) {
+    return min(min(x, y), z);
+}
+
+static uint64_t closest(uint64_t x, uint64_t c) {
+    constexpr uint64_t MOD = (0x1ul << 32);
+    u64 d1 = x - c, d2 = x + MOD - c, d3 = x - MOD - c;
+    if(x < c) {
+        d1 = c - x;
+    }
+    if(x - MOD < c) {
+        d3 = c - (x - MOD);
+    }
+    u64 min = min3(d1, d2, d3);
+    if(min == d1) return x;
+    else if(min == d2) return x + MOD;
+    else if(min == d3) return x - MOD;
+    else {
+        assert(0 && "should never touch here");
+    }
+    return 0;
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +56,12 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    WrappingInt32 temp(n - isn);
+    uint32_t temp_raw = temp.raw_value();
+
+    uint64_t mask = (static_cast<u64> (UINT32_MAX) << 32);
+    uint64_t high_checkpoint = checkpoint & mask;
+    uint64_t x = high_checkpoint | temp_raw;
+
+    return closest(x, checkpoint);
 }
